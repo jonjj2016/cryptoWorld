54243b5bd73a9601e4442695733f2017da0548c7
jonjj2016 <jonmartirosyan1988@gmail.com>
2021-09-25 17:49:18 +0800

    consuming api

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,10 +1,10 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query';
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
 const cryptoHeaders = {
     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
     'x-rapidapi-key': '80126376b4mshbd6b74ee3a99415p19c81ajsn94fb4f01e676'
 };
-const baseUrl = 'https://coinranking1.p.rapidapi.com/exchanges';
+const baseUrl = 'https://coinranking1.p.rapidapi.com/';
 
 const createRequest = (url) => ({url, headers: cryptoHeaders});
 
@@ -22,3 +22,4 @@ export const  cryptoApi = createApi({
 export const {
     useGetCryptosQuery,
 } = cryptoApi;
+// https://coinranking1.p.rapidapi.com/coins
